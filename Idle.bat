feac043422bccde83dee53258fd0318b02dff10a
Atsuko <90731676+DownLyfter@users.noreply.github.com>
2022-07-20 00:01:13 -0700

    Add files via upload

---
+++ Idle.bat
@@ -0,0 +1,37 @@
+cd C:\Users\Matoi\Documents\idle_master_extended_v1.7
+start steam-idle.exe 311210
+start steam-idle.exe 774171 
+start steam-idle.exe 282800
+start steam-idle.exe 945360
+start steam-idle.exe 238460
+start steam-idle.exe 1159640
+start steam-idle.exe 960090
+start steam-idle.exe 202970
+start steam-idle.exe 476600
+start steam-idle.exe 884660
+start steam-idle.exe 639790 
+start steam-idle.exe 859680
+start steam-idle.exe 639780
+start steam-idle.exe 248330
+start steam-idle.exe 698780
+start steam-idle.exe 377160
+start steam-idle.exe 4000
+start steam-idle.exe 271590
+start steam-idle.exe 1168170
+start steam-idle.exe 339800
+start steam-idle.exe 326480
+start steam-idle.exe 424840
+start steam-idle.exe 619700
+start steam-idle.exe 562010
+start steam-idle.exe 431960
+start steam-idle.exe 218620
+start steam-idle.exe 851100
+start steam-idle.exe 945360
+start steam-idle.exe 1159640
+start steam-idle.exe 1454400
+start steam-idle.exe 1276390
+start steam-idle.exe 893520
+start steam-idle.exe 251570
+start steam-idle.exe 204360
+start steam-idle.exe 930210
+
